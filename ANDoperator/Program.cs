50d6a0c9818bdb54aec54b00a817e1b0d527f660
Aleksandr Bychkov <aleksandr.bychkov@tktk.ee>
2023-11-28 21:21:56 +0200

    first commit

---
+++ ANDoperator/Program.cs
@@ -0,0 +1,21 @@
+﻿//Math >= 90, biology >= 90, chemistry >= 90
+
+int math, bio, che;
+
+Console.WriteLine("Enter your math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your biology result:");
+bio = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your chemistry result:");
+che = Int32.Parse(Console.ReadLine());
+
+if (math >= 90 && bio >= 90 && che >= 90)
+{
+    Console.WriteLine("Congratulations! You got accepted!");
+}
+else
+{
+    Console.WriteLine("Your application can not be approved.");
+}
---
+++ ORoperator/Program.cs
@@ -0,0 +1,23 @@
+﻿//math OR chemistry >= 90
+
+using System.Net.NetworkInformation;
+
+int math, bio, che;
+
+Console.WriteLine("Enter your math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your biology result:");
+bio = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your chemistry result:");
+che = Int32.Parse(Console.ReadLine());
+
+if ((math >= 90 || che >= 90) || (math >= 90 && bio >= 90) || (che >= 90 && bio >= 90))
+{
+    Console.WriteLine("Congratulations! You got accepted!");
+}
+else
+{
+    Console.WriteLine("Your application can not be approved.");
+}
